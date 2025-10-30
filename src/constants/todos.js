ebe93fd556e11cb40e3fb549fdec1bc7d819aabe
Robert Gizzatullin <gizzatullin.robert@mail.ru>
2025-10-30 13:42:18 +0300

    fix CONST API_URL

--- src/constants/todos.js
+++ src/constants/todos.js
@@ -1,2 +1,2 @@
 export const LOCAL_STORAGE_KEY = 'todos'
-export const API_URL = 'https://67fe07ca3da09811b1774cfc.mockapi.io/users'
+export const API_URL = 'https://67fe07ca3da09811b1774cfc.mockapi.io/todos'
